170d99aa9e9b27705388b18a09b8692c594a2065
NIKITA TEWARY <62873522+NikitaTewary@users.noreply.github.com>
2021-02-26 15:56:29 +0530

    Add files via upload

---
+++ Python/constants.py
@@ -0,0 +1,31 @@
+SIZE = 400
+GRID_LEN = 4
+GRID_PADDING = 10
+
+BACKGROUND_COLOR_GAME = "#dadaee"
+BACKGROUND_COLOR_CELL_EMPTY = "#1a1818"
+
+BACKGROUND_COLOR_DICT = {2: "#ede8e8", 4: "#f0b4b4", 8: "#f07a7a",
+                         16: "#e64747", 32: "#4d0202", 64: "#e500ed",
+                         128: "#40003d", 256: "#1a02f2", 512: "#070140",
+                         1024: "#00e6e6", 2048: "#004747",
+
+                         4096: "#eee4da", 8192: "#edc22e", 16384: "#f2b179",
+                         32768: "#f59563", 65536: "#f67c5f", }
+
+CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
+                   32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
+                   256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2",
+                   2048: "#f9f6f2",
+
+                   4096: "#776e65", 8192: "#f9f6f2", 16384: "#776e65",
+                   32768: "#776e65", 65536: "#f9f6f2", }
+
+FONT = ("Verdana", 40, "bold")
+
+
+
+KEY_UP = "'w'"
+KEY_DOWN = "'s'"
+KEY_LEFT = "'a'"
+KEY_RIGHT = "'d'"
